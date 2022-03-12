f7fb0e7cd1bfec7b97846fab5b6c4e90409765a4
박영준 <arong@bag-yeongjun-ui-MacBookAir.local>
2022-03-12 14:39:50 +0900

    base struct add

---
+++ base/base.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <climits>
+
+#define ll long long
+#define MAX 1001
+
+using namespace std;
+
+void init();
+void input();
+void solve();
+
+void init()
+{
+
+}
+
+void input()
+{
+
+}
+
+void solve()
+{
+
+}
+
+int main()
+{
+    init();
+    input();
+    solve();
+    return 0;
+}

b93b979116a08a9277a9c49c4a1b669bf429d314
박영준 <qkrdudwns98@soongsil.ac.kr>
2023-07-24 23:14:05 +0900

    mac

--- base/tmp.cpp
+++ base/tmp.cpp
@@ -1,12 +1,102 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <set>
+#include <queue>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
+void makeWaiting(const vector<vector<int> >& reqs, int k);
+int getWaitTime(const int type, const int numberofMento);
+void dfs(const int, const int, vector<int>&);
 
-vector<set<int> > woods;
+vector<vector<pair<int, int> > > waiting; // waiting[type] = {startTime, endTime}
+vector<vector<int> > memo;
+int answer, N, K;
 
-int main(){
-    woods.resize(5, set<int>());
-    return 0;
+void dfs(const int sum, const int depth, vector<int>& v){
+
+    if(sum > N) return;
+    if(depth == K && sum < N) return;
+    
+    // 자연수를 k개 골랐고, 그 수의 합이 sum임
+    if(depth == K && sum == N){
+        // v[i] = i번 type에서 사용할 Mento의 수
+        int tmp = 0;
+        for(int i=0; i<v.size(); i++){
+            int type = i+1;
+            int numberofMento = v[i];
+            tmp += memo[type][numberofMento];
+        }
+        answer = min(answer, tmp);
+        return;
+    }
+    
+    for(int i=1; i<=N; i++){
+        v.push_back(i);
+        dfs(sum + i, depth + 1, v);
+        v.pop_back();
+    }
+}
+
+
+int solution(int k, int n, vector<vector<int>> reqs) {
+    answer = 987654321;
+    makeWaiting(reqs, k);
+    memo.resize(k+1, vector<int>(n+1, 0));
+    N = n;
+    K = k;
+    
+    // memo[type][i] = type에 멘토가 i명 배치되었을 때 걸리는 대기 시간
+    for(int type=1; type<=k; type++){
+        for(int i=1; i<=n; i++){
+            memo[type][i] = getWaitTime(type, i);
+            if(memo[type][i] == 0) break;
+        }
+    }
+    
+    /*
+    for(int type=1; type<=k; type++){
+        for(int i=1; i<=n; i++){
+            cout << memo[type][i] << ' '; 
+        }
+        cout << endl;
+    }
+     */
+    
+    // 1이상인 자연수 k개로 이루어진 덧셈 중 합이 n이 되는 수
+    vector<int> v;
+    dfs(0, 0, v);
+    return answer;
+}
+
+
+int getWaitTime(const int type, const int numberofMento){
+    int waitTime = 0;
+    int currentTime = 0;
+    priority_queue<int> pq;
+
+    for(int i=0; i<waiting[type].size(); i++){
+        int start = waiting[type][i].first;
+        int duration = waiting[type][i].second;
+
+        while(pq.size() >= numberofMento){
+            currentTime = -pq.top();
+            pq.pop();
+        }
+        currentTime = max(currentTime, start);
+        waitTime += (currentTime - start);
+        pq.push(-(currentTime + duration));
+    }
+
+    return waitTime;
+}
+
+void makeWaiting(const vector<vector<int> >& reqs, int k){
+    waiting.resize(k+1, vector<pair<int, int> >());
+    for(int i=0; i<reqs.size(); i++){
+        int startTime = reqs[i][0];
+        int duration = reqs[i][1];
+        int type = reqs[i][2];
+        waiting[type].push_back({startTime, duration});
+    }   
 }
--- base/tmp2.cpp
+++ base/tmp2.cpp
@@ -1,76 +1,39 @@
-#include <string>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-typedef struct node{
-    int y1, x1, y2, x2, dir;
-    node(int y1, int x1, int y2, int x2, int d): y1(y1), x1(x1), y2(y2), x2(x2), dir(d) {};
-};
-
-int solution(vector<vector<int>> board) {
-    int answer = 0;
-    int n = board.size();
-    bool visited[100][100][2] = {false, }; // 0은 가로, 1은 세로
-    queue<node> q;
-    q.push({0, 0, 0, 1, 0});
+int getWaitTime(const int type, const int numberofMento){
+    int waitTime = 0;
+    int currentTime = 0;
+    priority_queue<int> pq; // 상담이 일찍 끝나는 순서대로. first = 끝나는 시간, second = 인덱스
     
-    while(!q.empty()){
-        int qs = q.size();
-        while(qs--){
-            node here = q.front();
-            q.pop();
+    for(int i=0; i<waiting[type].size(); i++){
+        int start = waiting[type][i].first; // 상담 요청 시간
+        int duration = waiting[type][i].second; // 걸리는 시간
+        
+        // 일하지 않는 멘토가 있음
+        if(pq.size() < numberofMento){
+            currentTime = start;
+            pq.push(-(currentTime + duration));
+        }
+        // 일하지 않는 멘토가 없음
+        else if(pq.size() == numberofMento){
+            int endTime = -pq.top(); // 가장 빨리 끝나는 상담
+            pq.pop(); // 상담을 끝냄
             
-            int y1 = here.y1;
-            int x1 = here.x2;
-            int y2 = here.y2;
-            int x2 = here.x2;
-            int dir = here.dir;
-            
-            if(y2 == n-1 && x2 == n-1) return answer;
-            if(visited[y2][x2][dir]) continue;
-            visited[y2][x2][dir] = true;
-            
-            // 상
-            node next = {y1-1, x1, y2-1, x2, dir};
-            if(0 <= y1-1 && !visited[y2-1][x2][dir] && board[y1-1][x1] == 0 && board[y2-1][x2] == 0){
-                q.push(next);
-                // 위로 이동했는데 현재 상태가 가로였을경우, 기존 위치에서 세로로 회전할 수 있음이 보장된다.
-                if(dir == 0){  
-                    q.push({y1-1, x1, y1, x1, 1});
-                    q.push({y2-1, x2, y2, x2, 1});
-                }
+            // 이미 끝난 상담이었다면
+            if(endTime <= currentTime){
+                currentTime = start;
+                pq.push(-(currentTime + duration));
             }
-            // 하
-            next = {y1+1, x1, y2+1, x2, dir};
-            if(y2+1 < n && !visited[y2+1][x2][dir] && board[y1+1][x1] == 0 && board[y2+1][x2] == 0){
-                q.push(next);
-                if(dir == 0){
-                    q.push({y1, x1, y1+1, x1, 1});
-                    q.push({y2, x2, y2+1, x2, 1});
+            // 아직 끝나지 않은 상담이었다면
+            else{
+                currentTime = endTime; // 일단 끝내주고
+                // 아직 시간이 남음.
+                if(currentTime <= start) currentTime = start;
+                // 아님
+                else{
+                    waitTime += (currentTime - start);
                 }
-            }
-            // 좌
-            next = {y1, x1-1, y2, x2-1, dir};
-            if(0 <= x1-1 && !visited[y2][x2-1][dir] && board[y1][x1-1] == 0 && board[y2][x2-1] == 0){
-                q.push(next);
-                // 좌측으로 이동했는데 현재 상태가 세로였을경우, 가로로 회전할 수 있음이 보장된다.
-                if(dir == 1){
-                    q.push({y1, x1-1, y1, x1, 0});
-                    q.push({y2, x2-1, y2, x2, 0});
-                }
-            }
-            // 우
-            next = {y1, x1+1, y2, x2+1, dir};
-            if(x2+1 < n && !visited[y2][x2+1][dir] && board[y1][x1+1] == 0 && board[y2][x2+1] == 0){
-                q.push(next);
-                if(dir == 1){
-                    q.push({y1, x1, y1, x1+1, 0});
-                    q.push({y2, x2, y2, x2+1, 0});
-                }
+                pq.push(-(currentTime + duration));
             }
         }
-        answer++;
     }
+    return waitTime;
 }

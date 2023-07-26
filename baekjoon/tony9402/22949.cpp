c9d06373d074312e581db6df0ca4a556576ceebe
박영준 <qkrdudwns98@soongsil.ac.kr>
2023-07-26 10:55:05 +0900

    Update 22949.cpp

--- baekjoon/tony9402/22949.cpp
+++ baekjoon/tony9402/22949.cpp
@@ -1,6 +1,7 @@
 // based problem:
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -10,6 +11,12 @@ using namespace std;
 void init();
 void input();
 void solve();
+vector<vector<char> > spin(const vector<vector<char> >& origin);
+
+int n, k;
+vector<vector<char> > bd;
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, -1, 0, 1};
 
 void init()
 {
@@ -18,13 +25,40 @@ void init()
 
 void input()
 {
-
+    cin >> k;
+    n = 4*k;
+    bd.resize(n, vector<char>(n, ' '));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> bd[i][j];
+        }
+    }
 }
 
 void solve()
 {
+    queue<pair<int, int> > peopleQ;
+    queue<pair<int, int> > spinQ; // 구역 first가 second번 돌았음.
+    
+}
+
+void paste(int y, int x, const vector<vector<char> >& origin){
 
 }
+
+// origin을 90도 회전시킴
+vector<vector<char> > spin(const vector<vector<char> >& origin){
+    int N = 2*k;
+    vector<vector<char> > ret = origin;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            ret[j][N-i-1] = origin[i][j]; // 시계방향 회전
+            // ret[i][j] = origin[j][N-i-1] 반시계방향 회전
+        }
+    }
+    return ret;
+}
+
 
 int main()
 {
